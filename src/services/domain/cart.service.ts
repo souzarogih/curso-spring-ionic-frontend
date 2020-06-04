7b2b8f0ef7a00cf993f5dd9e45c9cb7cb30a6bd6
Higor Souza <higor.andrade@hotmail.com>
2020-06-03 23:41:08 -0300

    Terminando as funcionalidades do carrinho

--- src/services/domain/cart.service.ts
+++ src/services/domain/cart.service.ts
@@ -23,6 +23,7 @@ export class CartService {
         return cart;
     }
 
+    //Adiciona um produto no carrinho
     addProduto(produto: ProdutoDTO) : Cart {
         let cart = this.getCart();
         let position = cart.items.findIndex(x => x.produto.id == produto.id);
@@ -31,5 +32,51 @@ export class CartService {
         }
         this.storage.setCart(cart);
         return cart;
+    }
+
+    //Remove um produto do carrinho
+    removeProduto(produto: ProdutoDTO) : Cart {
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if (position != -1) {
+            cart.items.splice(position, 1);
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    //Incrementa a quantidade de produtos no carrinho
+    increaseQuantity(produto: ProdutoDTO) : Cart {
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if (position != -1) {
+            cart.items[position].quantidade++;
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    //Decrementa a quantidade de produtos no carrinho
+    decreaseQuantity(produto: ProdutoDTO) : Cart {
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if (position != -1) {
+            cart.items[position].quantidade--;
+            if (cart.items[position].quantidade < 1) {
+                cart = this.removeProduto(produto);
+            }
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    //Somar o valor total do carrinho
+    total() : number {
+        let cart = this.getCart();
+        let sum = 0;
+        for (var i=0; i<cart.items.length; i++) {
+            sum += cart.items[i].produto.preco * cart.items[i].quantidade;
+        }
+        return sum;
     }
 }
