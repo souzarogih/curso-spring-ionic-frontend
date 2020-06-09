a56e2a9caad8c764eb65d178c7bf8b0a0859979a
Higor Souza <higor.andrade@hotmail.com>
2020-06-08 21:49:58 -0300

    Fazendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -28,6 +28,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
@@ -71,4 +75,18 @@ export class ProfilePage {
     });
   }
 
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response => {
+        this.picture = null;
+        this.loadData();
+      },
+      error => {
+
+      });
+  }
+
+  cancel() {
+    this.picture = null;
+  }
 }
